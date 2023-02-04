80f9342d622231d9795b4dc99fca7dd4e5119c92
Grant Benson <grantjbenson@icloud.com>
2023-02-04 16:40:16 -0500

    implemented camera script
    camera now follows players x and y position with a smoothening effect. The stopping of the camera will be through the player (as the camera can't move without them)

---
+++ DigDeep/DigDeepRootMovement/Assets/CameraFollowScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    [SerializeField] Transform playerPosition;
+
+    private float xMin, xMax;
+    private float camY,camX;
+    private float camOrthsize;
+    private float cameraRatio;
+    private Camera mainCam;
+    private Vector3 smoothPos;
+    public float smoothSpeed = 0.5f;
+    private GameObject player;
+    //private Transform playerPos;
+
+    private void Start()
+    {
+
+        mainCam = GetComponent<Camera>();
+        camOrthsize = mainCam.orthographicSize;
+        cameraRatio = (xMax + camOrthsize) / 2.0f;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //Debug.Log("Player position is now in scene, assigning camera position");
+        camY = playerPosition.position.y;
+        camX = playerPosition.position.x;
+        smoothPos = Vector3.Lerp(this.transform.position, new Vector3(camX, camY, this.transform.position.z), smoothSpeed);
+        this.transform.position = smoothPos;
+
+        
+    }
+
+}
+
