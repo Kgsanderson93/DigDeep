fe49d4d9a92bf04a0c5ce039f73c0b578e4de277
Grant Benson <grantjbenson@icloud.com>
2023-02-05 14:10:46 -0500

    copy pasted coordinate script

---
+++ DigDeep/DigDeepRootMovement/Assets/Coordinate.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets
+{
+
+
+    public class Coordinate :ScriptableObject
+
+     {
+    private int x;
+    private int y;
+    bool _itemoOccupy;
+    private GameObject _itemtohold;
+    private GameObject _tile;
+
+    public Coordinate()
+    {
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public void setTile(GameObject tile)
+    {
+        this._tile=tile;
+    }
+
+    public void GiveItem(GameObject item)
+    {
+        _itemoOccupy = true;
+        _itemtohold = item;
+    }
+
+    public GameObject GetItem()
+    {
+        return _itemtohold;
+    }
+
+    public int GetX()
+    {
+        return x;
+    }
+
+    public int GetY()
+    {
+        return y;
+        
+    }
+
+    public GameObject GetTile()
+    {
+        return _tile;
+    }
+
+    public bool ReturnEmpty()
+    {
+        return this._itemoOccupy;
+    }
+    }
+}
